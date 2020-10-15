4a0d7faa20bdf6e38d308b92d7951c2c2a3f92fc
Stanislao <stanislaoweb@libero.it>
2020-10-15 15:45:03 +0200

    ticket

---
+++ js-jsnack3/main.js
@@ -0,0 +1,13 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un
+// numero. Il programma stampa la somma di tutti i numeri
+// inseriti.
+
+var insertNumber = prompt('Inserisci un numero')
+
+for (var i = 0; i < insertNumber.length; i++) {
+  if (insertNumber.length < 5) {
+    insertNumbe
+  }
+}
+
+// insertNumber.push(insertNumber);
