fa4217a228120270848dae0d10d6a6c63ff25915
Stanislao <stanislaoweb@libero.it>
2020-10-16 17:33:06 +0200

    Finito snack 7

--- js-snack7/main.js
+++ js-snack7/main.js
@@ -1,9 +1,20 @@
  // Calcola la somma e la media dei numeri da 1 a 10.
 
-
 var somma = 0;
 
-for (var i = 1; i <= 10; i++) {
+var massimo = 10;
 
+var media = 0;
+
+for (var i = 1; i <= massimo; i++) {
+
+  somma = somma + i;
+
+
+  console.log('Questo è il valore di somma ad ogni i: ' +  somma);
 
 }
+
+console.log('Questa è la somma dei numeri da 1 a 10: ' + somma);
+
+console.log('Questa è la media dei numeri da 1 a 10: ' + somma / massimo);
